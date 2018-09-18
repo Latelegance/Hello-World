8febdd78ecacddba90a3f2b43ba5bb734dab775c
Latelegance <Latelegance@Outlook.com>
2018-09-18 13:29:46 +0800

    finish numbers in C#

---
+++ 2-C# 中的数字/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+
+namespace NumbersInCSharp
+{
+    class Program
+    {
+        static void WorkingWithIntegers()
+        {
+            int a = 18;
+            int b = 6;
+            int c = a + b;
+            Console.WriteLine(c);
+
+            c = a - b;
+            Console.WriteLine(c);
+            c = a * b;
+            Console.WriteLine(c);
+            c = a / b;
+            Console.WriteLine(c);
+
+            c = a + b - 12 * 17;
+            Console.WriteLine(c);
+        }
+        static void OrderPrecedence()
+        {
+            int a = 5;
+            int b = 4;
+            int c = 2;
+            int d = a + b * c;
+            Console.WriteLine(d);
+
+            d = (a + b) * c;
+            Console.WriteLine(d);
+
+            d = (a + b) - 6 * c + (12 * 4) / 3 + 12;
+            Console.WriteLine(d);
+
+            int e = 7;
+            int f = 4;
+            int g = 3;
+            int h = (e  + f) / g;
+            Console.WriteLine(h);
+        }
+        static void TestLimits()
+        {
+            int a = 7;
+            int b = 4;
+            int c = 3;
+            int d = (a + b) / c;
+            int e = (a + b) % c;
+            Console.WriteLine($"quotient: {d}");
+            Console.WriteLine($"remainder: {e}");
+
+            int max = int.MaxValue;
+            int min = int.MinValue;
+            Console.WriteLine($"The range of integers is {min} to {max}");
+
+            int what = max + 3;
+            Console.WriteLine($"An example of overflow: {what}");
+        }
+        static void WorkWithDoubles()
+        {
+            double a = 5;
+            double b = 4;
+            double c = 2;
+            double d = (a + b) / c;
+            Console.WriteLine(d);
+
+            double e = 19;
+            double f = 23;
+            double g = 8;
+            double h = (e  + f) / g;
+            Console.WriteLine(h);
+
+            double max = double.MaxValue;
+            double min = double.MinValue;
+            Console.WriteLine($"The range of double is {min} to {max}");
+
+            double third = 1.0 / 3.0;
+            Console.WriteLine(third);
+
+        }
+        static void WorkWithDecimals()
+        {
+            decimal min = decimal.MinValue;
+            decimal max = decimal.MaxValue;
+            Console.WriteLine($"The range of the decimal type is {min} to {max}");
+
+            double a = 1.0;
+            double b = 3.0;
+            Console.WriteLine(a / b);
+
+            decimal c = 1.0M;
+            decimal d = 3.0M;
+            Console.WriteLine(c / d);
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n探索整数数学运算：");
+                WorkingWithIntegers();
+
+            Console.WriteLine("\n探索运算顺序：");
+                OrderPrecedence();
+
+            Console.WriteLine("\n探索整数运算精度和限值：");
+                TestLimits();
+
+            Console.WriteLine("\n使用双精度类型：");
+                WorkWithDoubles();
+
+            Console.WriteLine("\n使用固定点类型：");
+                WorkWithDecimals();
+            
+            Console.WriteLine("\n挑战：");
+                double R = 2.50;
+                Console.WriteLine(Math.PI * R * R);
+            
+            Console.WriteLine("\n已完成“C# 中的数字”快速入门教程。\n");
+        }
+    }
+}
