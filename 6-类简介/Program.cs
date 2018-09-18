647f147e8686b4e92259910de08eda93804f77c2
Latelegance <Latelegance@Outlook.com>
2018-09-18 17:20:32 +0800

    Modify annotation format

--- 6-类简介/Program.cs
+++ 6-类简介/Program.cs
@@ -15,27 +15,29 @@ namespace classes
             account.MakeDeposit(100, DateTime.Now, "friend paid me back");
             Console.WriteLine(account.Balance);
 
-            // // Test that the initial balances must be positive:
-            // try
-            // {
-            //     var invalidAccount = new BankAccount("invalid", -55);
-            // }
-            // catch (ArgumentOutOfRangeException e)
-            // {
-            //     Console.WriteLine("Exception caught creating account with negative balance");
-            //     Console.WriteLine(e.ToString());
-            // }
+            /* Test that the initial balances must be positive:
+            try
+            {
+                var invalidAccount = new BankAccount("invalid", -55);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine("Exception caught creating account with negative balance");
+                Console.WriteLine(e.ToString());
+            }
+            */
 
-            // // Test for a negative balance
-            // try
-            // {
-            //     account.MakeWithdrawal(750, DateTime.Now, "Attempt to overdraw");
-            // }
-            // catch (InvalidOperationException e)
-            // {
-            //     Console.WriteLine("Exception caught trying to overdraw");
-            //     Console.WriteLine(e.ToString());
-            // }
+            /* Test for a negative balance
+            try
+            {
+                account.MakeWithdrawal(750, DateTime.Now, "Attempt to overdraw");
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine("Exception caught trying to overdraw");
+                Console.WriteLine(e.ToString());
+            }
+            */
 
             Console.WriteLine("\n挑战 - 记录所有交易：");
             Console.WriteLine(account.GetAccountHistory());
